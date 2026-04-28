e189fc6cdb8b09af9a1e0f216b43d5d4a7d1f657
viswanath613 <sandrilla.phd@gmail.com>
2026-04-28 14:46:24 +0530

    Create cnn.py

---
+++ data/raw/src/models/cnn.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+
+def build_model(input_dim):
+    model = tf.keras.Sequential([
+        tf.keras.layers.Dense(64, activation='relu', input_shape=(input_dim,)),
+        tf.keras.layers.Dense(32, activation='relu'),
+        tf.keras.layers.Dense(1, activation='sigmoid')
+    ])
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    return model
